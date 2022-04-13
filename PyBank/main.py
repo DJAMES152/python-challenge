1e934e74d94b7021c6f804d1d88dadf676e7d8a6
DJAMES152 <davejames20@hotmail.com>
2022-04-12 23:25:25 -0500

    Final Product of PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,9 +1,10 @@
 #Importing Libraries
 import os
 import csv
+from pathlib import Path
 
 #path to collect data from the budget data excel sheet
-budget_csv = os.path.join('Resources','budget_data.csv')
+budget_csv = Path('Resources','budget_data.csv')
 
 #setting variables and empty lists
 months = []
@@ -38,6 +39,7 @@ max_decrease = min(mon_profit_change)
 month_increase = mon_profit_change.index(max(mon_profit_change)) +1
 month_decrease = mon_profit_change.index(min(mon_profit_change)) +1
 
+#setting print statements
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {len(months)}")
@@ -45,3 +47,23 @@ print(f"Total: ${sum(netprofit)}")
 print(f"Average Change: {round(sum(mon_profit_change)/len(mon_profit_change), 2)}")
 print(f"Greatest Increase in Profits: {months[month_increase]} (${(str(max_increase))})")
 print(f"Greatest Decrease in Profits: {months[month_decrease]} (${(str(max_decrease))})")
+
+#setting output text file
+fin_analysis_results = Path("Analysis", "fin_analysis_results.txt")
+
+with open(fin_analysis_results,"w") as file:
+    
+#creating text file of results
+    file.write("Financial Analysis")
+    file.write("\n")
+    file.write("----------------------------")
+    file.write("\n")
+    file.write(f"Total Months: {len(months)}")
+    file.write("\n")
+    file.write(f"Total: ${sum(netprofit)}")
+    file.write("\n")
+    file.write(f"Average Change: {round(sum(mon_profit_change)/len(mon_profit_change), 2)}")
+    file.write("\n")
+    file.write(f"Greatest Increase in Profits: {months[month_increase]} (${(str(max_increase))})")
+    file.write("\n")
+    file.write(f"Greatest Decrease in Profits: {months[month_decrease]} (${(str(max_decrease))})")
