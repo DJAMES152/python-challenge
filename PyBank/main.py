e1acb2072277d9dcd6aa3b4c7f2f79acd9d0626b
DJAMES152 <davejames20@hotmail.com>
2022-04-12 21:41:10 -0500

    created variables and formulas for max and min profit changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -30,3 +30,18 @@ with open(budget_csv) as budget:
         #taking difference between two seperate months calculated into the profit change of both
         mon_profit_change.append(netprofit[i+1]-netprofit[i])
 
+#creating variables for maximum and minimum monthly profit changes
+max_increase = max(mon_profit_change)
+max_decrease = min(mon_profit_change)
+
+#giving variable to maximium and minimum index list to grab data from
+month_increase = mon_profit_change.index(max(mon_profit_change)) +1
+month_decrease = mon_profit_change.index(min(mon_profit_change)) +1
+
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {len(months)}")
+print(f"Total: ${sum(netprofit)}")
+print(f"Average Change: {round(sum(mon_profit_change)/len(mon_profit_change), 2)}")
+print(f"Greatest Increase in Profits: {months[month_increase]} (${(str(max_increase))})")
+print(f"Greatest Decrease in Profits: {months[month_decrease]} (${(str(max_decrease))})")
