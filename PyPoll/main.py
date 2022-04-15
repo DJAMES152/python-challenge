25a5e18a36c1fb0d374b5e0d02325ccfbdc50925
DJAMES152 <davejames20@hotmail.com>
2022-04-14 19:31:18 -0500

    created readable csv variable and if statements for votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -12,3 +12,28 @@ khan_vote = 0
 correy_vote = 0
 li_vote = 0
 otooley_vote = 0
+
+#opening readable csv file
+with open(csv_file_path,newline="", encoding="utf-8") as campaign:
+
+    #creating usuable csv variable
+    csvreader = csv.reader(campaign,delimiter=",") 
+
+    #skipping header row
+    header = next(csvreader)     
+
+    #go through each row of data
+    for row in csvreader: 
+
+        #counting all votes in dataset
+        total_vote +=1
+
+        #counting one vote per individual candidate accordingly using an if statement
+        if row[2] == "Khan": 
+            khan_vote +=1
+        elif row[2] == "Correy":
+            correy_vote +=1
+        elif row[2] == "Li": 
+            li_vote +=1
+        elif row[2] == "O'Tooley":
+            otooley_vote +=1
